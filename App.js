924fb07273ee12c272e2c6c5ec188074c91f6eb1
Kunj Dhami <kunjdhami26@gmail.com>
2020-11-29 23:14:27 +0530

    3rd commit

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
-import {Text ,StyleSheet, View} from 'react-native';
-import {RNCamera} from 'react-native-camera';
+import {Text ,StyleSheet, View, TouchableOpacity} from 'react-native';
+import {RNCamera} from 'react-native-camera'
 
-const pendingView = () => (
-  <View style={{flexGrow: 1, justifyContent: 'center', alignItems: 'center'}}>
+const PendingView = () => (
+  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
     <Text style={{fontSize: 30, color: 'red'}}>Loading...</Text>
   </View>
-);
+)
 
 const App = () => {
   const [image, setImage] = useState(null);
@@ -17,18 +17,97 @@ const App = () => {
       const data = await camera.takePictureAsync(options)
       setImage(data.uri)
     } catch (error) {
-      console.warn(error);
+      console.warn(error)
     }
-  }
+  };
   return(
-    <View>
-      <Text>keep it up, da fak</Text>
+    <View style={styles.container}>
+      {image ? (
+        <View style={styles.preview}>
+          <Text style={styles.camtext}>Here is your new profile pic</Text>
+          <Image style={styles.clicked} source={{uri: image, width: '100%', height:'80%'}} />
+          <Button
+          title="CLick new Image"
+          onPress={() => {
+            setImage(null)
+          }}
+          ></Button>
+        </View>
+        ) : (
+        <RNCamera
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          captureAudio={false}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: "Permission to use camera",
+            message: "longer text to use camera",
+            buttonPositive: "OK",
+            buttonNegative: "Cancel"
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: "Permission to use audio",
+            message: "longer text to use audio",
+            buttonPositive: "OK",
+            buttonNegative: "Cancel"
+          }}
+          >
+          {({camera, status}) => {
+            if(status !== 'READY') return <PendingView/>
+            return(
+              <View
+              style={{
+                flex: 0,
+                flexDirection: 'row',
+                justifyContent: "center"
+              }}
+              >
+                <TouchableOpacity
+                style={styles.capture}
+                onPress={() => takePicture(camera)}
+                >
+                  <Text>SNAP</Text>
+                </TouchableOpacity>
+              </View>
+            )
+          }}
+        </RNCamera>)
+      }
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flexGrow: 1,
+    flexDirection: 'column',
+    backgroundColor: '#0a79df'
+  },
+  preview: {
+    flexGrow: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: 'white',
+    padding: 20,
+    alignSelf: 'center'
+  },
+  camtext: {
+    backgroundColor: "#3498DB",
+    color: "#FFFFFF",
+    marginBottom: 10,
+    width: "100%",
+    textAlign: "center",
+    paddingVertical: 20,
+    fontSize: 25
+  },
+  clicked: {
+    width: 300,
+    height: 300,
+    borderRadius: 150
+  }
 });
 
 export default App;
